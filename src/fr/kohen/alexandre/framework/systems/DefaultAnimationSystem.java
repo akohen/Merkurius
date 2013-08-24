5dfc8400e3bd45a3abd451286898020d911c374f
Alexandre <alexandre@kohen.fr>
2013-08-24 14:44:15 +0200

    Animation system is less horrible

--- src/fr/kohen/alexandre/framework/systems/DefaultAnimationSystem.java
+++ src/fr/kohen/alexandre/framework/systems/DefaultAnimationSystem.java
@@ -9,13 +9,11 @@ import com.artemis.systems.EntityProcessingSystem;
 import com.badlogic.gdx.math.Vector2;
 
 import fr.kohen.alexandre.framework.base.C;
-import fr.kohen.alexandre.framework.base.C.STATES;
 import fr.kohen.alexandre.framework.components.EntityState;
 import fr.kohen.alexandre.framework.components.Transform;
 import fr.kohen.alexandre.framework.components.Velocity;
 import fr.kohen.alexandre.framework.components.VisualComponent;
 import fr.kohen.alexandre.framework.model.Visual;
-import fr.kohen.alexandre.framework.model.visuals.BoxVisual;
 import fr.kohen.alexandre.framework.systems.interfaces.AnimationSystem;
 
 /**
@@ -29,7 +27,6 @@ public class DefaultAnimationSystem extends EntityProcessingSystem implements An
 	protected ComponentMapper<EntityState> 		stateMapper;
 	protected ComponentMapper<Velocity> 		velocityMapper;
 	protected Map<Entity,Visual>				visuals;
-	protected BoxVisual							visualMouse;
 	
 	
 	@SuppressWarnings("unchecked")
@@ -53,9 +50,9 @@ public class DefaultAnimationSystem extends EntityProcessingSystem implements An
 	}
 	
 	protected void setCurrentAnim(VisualComponent visual, EntityState state, Vector2 speed) {
-		if( state.getState() == STATES.MOVING ) {
+		if( speed.len() > 1 ) {
 			visual.currentAnimationName = getAnim(visual, speed) ;
-		} else if( state.getState() == STATES.IDLE ) {
+		} else {
 			visual.currentAnimationName = idleAnim(visual);
 		}
 	}
@@ -66,26 +63,27 @@ public class DefaultAnimationSystem extends EntityProcessingSystem implements An
 		if( speed.len() < 1 )
 			return idleAnim(visual);
 			
-		if( speed.x < 0 ) {
-			if( speed.y < 0 )
+		if( speed.x < 0.1f ) {
+			if( speed.y < 0.1f )
 				currentAnim = C.WALK_DOWN_LEFT;
-			else if( speed.y > 0 )
+			else if( speed.y > 0.1f )
 				currentAnim = C.WALK_UP_LEFT;
 			else 
 				currentAnim = C.WALK_LEFT;
-		} else if( speed.x > 0 ) {
-			if( speed.y < 0 )
+		} else if( speed.x > 0.1f ) {
+			if( speed.y < 0.1f )
 				currentAnim = C.WALK_DOWN_RIGHT;
-			else if( speed.y > 0 )
+			else if( speed.y > 0.1f )
 				currentAnim = C.WALK_UP_RIGHT;
 			else 
 				currentAnim = C.WALK_RIGHT;
 		} else {
-			if( speed.y > 0 )
+			if( speed.y > 0.1f )
 				currentAnim = C.WALK_UP;
 			else 
 				currentAnim = C.WALK_DOWN;
-		}
+		}		
+		
 		return currentAnim;
 	}
 	
@@ -112,7 +110,10 @@ public class DefaultAnimationSystem extends EntityProcessingSystem implements An
 			currentAnim = C.STAND_UP_LEFT;
 		} else if (currentAnim.equalsIgnoreCase(C.WALK_UP_RIGHT)) {
 			currentAnim = C.STAND_UP_RIGHT;
+		} else {
+			currentAnim = C.STAND_LEFT;
 		}
+		
 		return currentAnim;
 	}
 	
